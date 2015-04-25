faebbc60ef7a3e2117551a7552cae9ee7806ad8c
christine <wangjl0512@gmail.com>
2015-04-25 23:19:15 +0800

    Add back button; Add validation on next button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /**
  * Created by christine on 3/31/15.
  */
-/*var allQuestions = [
+var allQuestions = [
     {
         question: "The moon is a ?",
         choice: [
@@ -57,16 +57,42 @@
         answer: 2
 
     }
-];*/
+];
+
+// Event Util
+var EventUtil = {
+    addEvent : function (elem, type, handler) {
+        if (addEventListener) {
+            elem.addEventListener(type, handler, false);
+        } else if (attachEvent) {
+            elem.attachEvent(type, handler);
+        } else {
+            elem[type] = handler;
+        }
+    },
+
+    removeEvent: function (elem, type, handler) {
+        if (removeEventListener) {
+            elem.removeEventListener(type, handler, false);
+        } else if (detachEvent) {
+            elem.detachEvent(type, handler);
+        } else {
+            elem[type] = null;
+        }
+    }
+
+};
 
 var elem_q = document.getElementById('question'),
     elem_next = document.getElementById('next'),
-    elem_score = document.getElementById('score'),
+    elem_back = document.getElementById('back'),
+    elem_error = document.getElementById('error'),
     num,
-    allQuestions = [];
+//    allQuestions = [],
+    results = [];
 
 window.onload = function(){
-    // get question through json file
+  /*  // get question through json file
     loadJSON(function(response){
         allQuestions = JSON.parse(response);
         // initialize
@@ -75,43 +101,63 @@ window.onload = function(){
         showQuestion();
 
         // handle with next button
-        elem_next.onclick = function () {
-            if (num < allQuestions.length - 1) {
-                num ++;
-                showQuestion();
-            } else {
-                clearNode(elem_q);
-                elem_q.innerHTML = '<p>Game Over</p><p>Your total score is ' + Number.parseInt(elem_score.innerText, 10) + '</p>';
-            }
-        };$
-    });
+        EventUtil.addEvent(elem_next, 'click', next);
 
+        // handle with back button
+        EventUtil.addEvent(elem_back, 'click', back);
+    }); */
+
+    // initialize
+    // show first question
+    num = 0;
+    showQuestion();
+
+    // handle with next button
+    EventUtil.addEvent(elem_next, 'click', next);
+
+    // handle with back button
+    EventUtil.addEvent(elem_back, 'click', back);
 
 }
 
-function checkAnswer () {
-    var elem_ul = document.getElementById('choices');
-    var elem_choices = elem_ul.getElementsByTagName('a');
-    var score = Number.parseInt(elem_score.innerText, 10);
-    for (var i = 0; i < elem_choices.length; i++) {
-        elem_choices[i].onclick = function (index) {
-            return (function () {
-                var answer = allQuestions[num].answer;
+function loadJSON (callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'questions.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == 200) {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send();
+}
 
-                // change selected answer to red if it is not the correct answer
-                if (answer != index) {
-                    elem_choices[index].style.color = 'red';
-                } else {
-                    elem_score.innerText = ++score;
-                }
+function next () {
+    // check whether question is answered
+    if (validateChoice()) {
+        // remove error
+        elem_error.innerHTML = "";
 
-                // set correct answer to green
-                elem_choices[answer].style.color = 'green';
-                elem_choices[answer].style.fontWeight = 'bold';
-                return false;
-            });
+        num ++;
+        // if the question is not the last question, show next question
+        if (num < allQuestions.length) {
+            showQuestion();
+        }
+        // else show the final score
+        else {
+            clearNode(elem_q);
+            elem_q.innerHTML = '<p>Game Over</p><p>Your total score is ' + getScore() + '</p>';
+            checkBtnVisibility();
+        }
+    } else {
+        elem_error.innerHTML = 'Please choose an answer.'
+    }
+}
 
-        }(i);
+function back () {
+    if (num > 0) {
+        num --;
+        showQuestion();
     }
 }
 
@@ -136,19 +182,83 @@ function showQuestion () {
     var choices = question.choice;
     for (var i = 0; i < choices.length; i++) {
         var elem_choice = document.createElement('li');
-        var elem_choice_a = document.createElement('a');
-        elem_choice_a.appendChild(document.createTextNode(choices[i]));
-        elem_choice_a.href = '#';
-        elem_choice.appendChild(elem_choice_a);
+        // checkbox
+        var elem_choice_checkbox = document.createElement('input');
+        var cId = num + '-' + i;
+        setAttributes(elem_choice_checkbox, {'type':'checkbox', 'class':'checkbox', 'id':cId});
+        elem_choice.appendChild(elem_choice_checkbox);
+
+        // choice content
+        var elem_choice_content = document.createTextNode(choices[i]);
+        elem_choice.appendChild(elem_choice_content);
+
         elem_choices.appendChild(elem_choice);
     }
     fragment.appendChild(elem_choices);
 
     elem_q.appendChild(fragment);
 
-    // bind click event on choices
-    checkAnswer();
+    var checkboxs = document.getElementsByClassName('checkbox');
 
+    // check button visibility
+    checkBtnVisibility();
+
+    // show checked answer
+    showCheckedAnswer(checkboxs);
+
+    // bind checkbox handler
+    for (var j = 0, len = checkboxs.length; j < len; j++) {
+        EventUtil.addEvent(checkboxs[j], 'click', recordAnswer);
+    }
+}
+
+function checkBtnVisibility () {
+    // if current question is the first question, disable the back button.
+    if (num == 0) {
+        elem_back.disabled = true;
+        return;
+    }
+
+    // if current page is not the question page, disable the back and next buttons.
+    if (num == allQuestions.length) {
+        elem_back.disabled = true;
+        elem_next.disabled = true;
+        return;
+    }
+
+    // for other situations, enable the back and next buttons.
+    elem_back.disabled = false;
+    elem_next.disabled = false;
+}
+
+function showCheckedAnswer (checkboxs) {
+    // check whether already had answered current question
+    for (var i = 0,len = results.length; i < len; i++) {
+        if (results[i].questionNum == num) {
+            checkboxs[results[i].answerNum].checked = true;
+        }
+    }
+}
+
+function recordAnswer () {
+    var parts = this.id.split('-');
+    var result = {
+        questionNum : parts[0],
+        answerNum : parts[1]
+    };
+    results.push(result);
+}
+
+function getScore () {
+    for (var i = 0, len = results.length; i < len; i++) {
+        var result = results[i],
+            score = 0;
+        console.log(result.answerNum, allQuestions[result.questionNum].answer);
+        if (result.answerNum == allQuestions[result.questionNum].answer) {
+            score += 10;
+        }
+    }
+    return score;
 }
 
 function clearNode (elem) {
@@ -157,14 +267,19 @@ function clearNode (elem) {
     }
 }
 
-function loadJSON (callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'questions.json', true);
-    xobj.onreadystatechange = function () {
-      if (xobj.readyState == 4 && xobj.status == 200) {
-          callback(xobj.responseText);
-      }
-    };
-    xobj.send();
+function validateChoice () {
+    var checkboxs = document.getElementsByClassName('checkbox');
+    for (var i = 0, len = checkboxs.length; i < len; i++) {
+        var thisCheckbox = checkboxs[i];
+        if (thisCheckbox.checked) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function setAttributes (elem, attrs) {
+    for (var key in attrs) {
+        elem.setAttribute(key, attrs[key]);
+    }
 }
